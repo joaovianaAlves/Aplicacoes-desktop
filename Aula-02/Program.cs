f59e650a6b9f3222b28c2186af20329a282b4c65
unisanta <unisanta@M312-03>
2023-02-13 20:19:23 -0300

    Teste

--- Aula-02/Program.cs
+++ Aula-02/Program.cs
@@ -1,11 +1,17 @@
-﻿string escreve;
+﻿static void Verificacao() {
+    string escreve;
+    escreve = "";
+    do
+    {
+        Console.WriteLine("Escreve QUE");
+        escreve = Console.ReadLine();
+    } while (escreve != "QUE");
 
-escreve = Console.ReadLine();
 
-do
-{
-    Console.WriteLine("Escreve QUE");
-    escreve = Console.ReadLine();
-} while (escreve != "QUE");
+    if (escreve == "QUE")
+    {
+        Console.WriteLine("Aceito");
+    }
+}
 
-Console.WriteLine("Aceito");
+Verificacao();
